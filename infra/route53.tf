e1a9ebcc12b4650f32ff695d27cc9cde5b8368fe
Emilija <emilija.zuk@gmail.com>
2025-08-02 18:01:13 +1000

    Stage 1 terraform deploy in AWS

---
+++ infra/route53.tf
@@ -0,0 +1,35 @@
+resource "aws_route53_zone" "main" {
+  # Remove count - zone exists in both stages
+  name = var.domain_name
+}
+
+output "route53_nameservers" {
+  value       = var.stage2 ? null : aws_route53_zone.main[0].name_servers
+  description = "Route53 name servers"
+}
+
+resource "aws_route53_record" "alias_root" {
+  count   = var.stage2 ? 1 : 0
+  zone_id = local.zone_id  
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn[0].domain_name
+    zone_id                = aws_cloudfront_distribution.cdn[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "alias_www" {
+  count   = var.stage2 ? 1 : 0
+  zone_id = local.zone_id  
+  name    = element(var.alternate_names, 0)
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn[0].domain_name
+    zone_id                = aws_cloudfront_distribution.cdn[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
